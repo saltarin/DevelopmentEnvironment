ada8ce6368da767f7bc664a6ad3c4add3ccae6a0
= <=>
2018-01-06 15:13:26 -0500

    setup express for development

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+const port = 3000;
+let app = new express();
+
+app.get('/',function(req,res){
+
+  res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.listen(port, function(error){
+  if(error)
+    console.log(error);
+
+  console.log('listen to ',port);
+  let serverUrl = 'http://localhost:' + port;
+  let browser = 'chrome';
+  console.log(`opening ${serverUrl} in ${browser}`);
+  open(serverUrl,browser);
+});
