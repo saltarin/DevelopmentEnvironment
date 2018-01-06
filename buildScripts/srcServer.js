ca7704b10772697abad79382fedf4cd7530192a6
= <=>
2018-01-06 15:23:20 -0500

    using localtunnel for development. ngrok(login), now(persisting web) and surge(static persistence) are good too

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,18 +5,18 @@ const open = require('open');
 const port = 3000;
 let app = new express();
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
 
-  res.sendFile(path.join(__dirname,'../src/index.html'));
+	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(error){
-  if(error)
-    console.log(error);
+app.listen(port, function (error) {
+	if (error)
+		console.log(error);
 
-  console.log('listen to ',port);
-  let serverUrl = 'http://localhost:' + port;
-  let browser = 'chrome';
-  console.log(`opening ${serverUrl} in ${browser}`);
-  open(serverUrl,browser);
+	console.log('listen to ', port);
+	let serverUrl = 'http://localhost:' + port;
+	let browser = 'chrome';
+	console.log(`opening ${serverUrl} in ${browser}`);
+	open(serverUrl, browser);
 });
